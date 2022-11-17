2f020a19d68c7fd6a1a70d74091d439e776d5f44
smitdumore <smit.dumore17@vit.edu>
2022-11-16 20:34:12 -0500

    added results dir and changed project name

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -27,17 +27,16 @@ using namespace std::chrono_literals;
 
 class MinimalPublisher : public rclcpp::Node {
  public:
-  MinimalPublisher()
-  : Node("minimal_publisher"), count_(0) {
+  MinimalPublisher() : Node("minimal_publisher"), count_(0) {
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
     timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+        500ms, std::bind(&MinimalPublisher::timer_callback, this));
   }
 
  private:
   void timer_callback() {
     auto message = std_msgs::msg::String();
-    message.data = "ENPM808X pub sub practice " + std::to_string(count_++);
+    message.data = "ENPM808X " + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
@@ -46,7 +45,7 @@ class MinimalPublisher : public rclcpp::Node {
   size_t count_;
 };
 
-int main(int argc, char * argv[]) {
+int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalPublisher>());
   rclcpp::shutdown();
