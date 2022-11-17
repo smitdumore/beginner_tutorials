b22d3349d060cf72acf812bd88ec31004e7c2ab2
smitdumore <smit.dumore17@vit.edu>
2022-11-16 19:51:58 -0500

    added client cpp file and modified talker node

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -19,34 +19,82 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "example_interfaces/srv/add_two_ints.hpp"
 
 using namespace std::chrono_literals;
-
-/* This example creates a subclass of Node and uses std::bind() to register a
- * member function as a callback from the timer. */
+size_t sum_;
 
 class MinimalPublisher : public rclcpp::Node {
- public:
-  MinimalPublisher()
-  : Node("minimal_publisher"), count_(0) {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-    timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
-  }
+public:
+  MinimalPublisher() : Node("minimal_publisher") {
 
- private:
+    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+
+    timer_ = this->create_wall_timer(
+        500ms, std::bind(&MinimalPublisher::timer_callback, this));
+
+    auto serviceCallbackPtr =
+        std::bind(&MinimalPublisher::add, this,
+                  std::placeholders::_1, std::placeholders::_2);
+    service_ = create_service<example_interfaces::srv::AddTwoInts>(
+        "add_two_ints", serviceCallbackPtr);
+  }
+private:
   void timer_callback() {
     auto message = std_msgs::msg::String();
-    message.data = "ENPM808X pub sub practice " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    message.data = std::to_string(sum_);
+    RCLCPP_INFO_STREAM(this->get_logger(), "Publishing:" << message.data);
     publisher_->publish(message);
   }
+
+  void add(const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
+        std::shared_ptr<example_interfaces::srv::AddTwoInts::Response> response)
+  {
+    response->sum = request->a + request->b;
+    sum_ = response->sum;
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\na: %ld" " b: %ld",
+                request->a, request->b);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%ld]", (long int)response->sum);
+  }
+
+  //members
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  size_t count_;
+  rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service_;
+  size_t sum;
 };
+/*
+void timer_callback() {
+    auto message = std_msgs::msg::String();
+    message.data = defaultMessage + std::to_string(count_++);
+    RCLCPP_INFO_STREAM(this->get_logger(), "Publishing:" << message.data);
+    publisher_->publish(message);
+}
 
+*/
 int main(int argc, char * argv[]) {
+  /**
+  rclcpp::init(argc, argv);
+  
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_server");
+
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_ = node->create_publisher<std_msgs::msg::String>("topic", 10);
+
+  rclcpp::TimerBase::SharedPtr timer_ = node->create_wall_timer(500ms, &timer_callback);
+
+  rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service =
+     node->create_service<example_interfaces::srv::AddTwoInts>("add_two_ints", &add);
+
+  while(rclcpp::ok())
+  {
+    auto message = std_msgs::msg::String();
+    message.data = "The sum is : " + std::to_string(sum_);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Publishing: '%s'", message.data.c_str());
+    publisher_->publish(message);
+    
+  }
+  */
+
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalPublisher>());
   rclcpp::shutdown();
